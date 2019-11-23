28acf4c64793e97bbe96f5272b63642f3abd13f0
Samuel Esposito <samuel-esp@hotmail.it>
2019-11-23 14:06:24 +0100

    Added UI elements and improved overall organization

---
+++ Discover/Controller/InitialViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InitialViewController.swift
+//  Discover
+//
+//  Created by Samuel Esposito on 23/11/2019.
+//  Copyright © 2019 Samuel Esposito. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
